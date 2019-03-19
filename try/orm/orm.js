fc0c2d0e91a7f25805fe9ad3e8032ade00725d41
alv67 <alv67.web@gmail.com>
2019-03-20 00:53:47 +0100

    ORM aggiunta Add & Remove record Nel test tecnologico ORM ora è stato aggiunta la prova di creazione ed eliminazione di record. È stato creato un database dedicato al "try" e la pagina di test visualizza tabella utilizzando Bootstrap.

--- try/orm/orm.js
+++ try/orm/orm.js
@@ -3,14 +3,15 @@ const typeorm = require('typeorm');
 const { createConnection, getRepository } = typeorm;
 
 
+
 createConnection().then(() => {
-  var commessa = {
-    matricola:   "12345",
-    tipo:        "TSI",
-    cliente:     "Nestlè",
-    riferimento: "Waterfall"
-  }
-  return getRepository("Commessa").save(commessa);
+  // var commessa = {
+  //   matricola:   "12345",
+  //   tipo:        "TSI",
+  //   cliente:     "Nestlè",
+  //   riferimento: "Waterfall"
+  // }
+  // return getRepository("Commessa").save(commessa);
 
 }).then(() => {
   console.log("Salvata correttamente!");
@@ -19,13 +20,52 @@ createConnection().then(() => {
 }).then(commesse => {
   console.log("Commesse :", commesse);
 
-  let ul = document.getElementById('matricoleList');
-  commesse.forEach(matricola => {
-    var li = document.createElement('li');
-    li.innerText = matricola.id + ". " + matricola.matricola;
-    ul.appendChild(li);
+  let tb = document.getElementById('matricoleList');
+  commesse.forEach(commessa => {
+    var tr = document.createElement('tr');
+    Object.keys(commessa).forEach(key => {
+      var td = document.createElement('td');
+      var txt = document.createTextNode(commessa[key]);
+      td.appendChild(txt);
+      tr.appendChild(td);
+    })
+    id = commessa["id"];
+    var x = document.createElement('button');
+    x.innerHTML = "X";
+    x.setAttribute("onclick","removeCommessa("+id+")");
+
+    x.className = "btn btn-outline-danger btn-sm"
+
+    // x.addEventListener("click", async () =>  {
+    //   console.log("Target:"+this);
+    //   // let rep = getRepository("Commessa");
+    //   // let toremove = await rep.findOne(id);
+    //   // await rep.remove(toremove);
+    // })
+    
+    tr.appendChild(x);
+
+    tb.appendChild(tr);
   });
 
 }).catch( error => {
   console.error("Error: ", error);
 });
+
+async function removeCommessa(id) {
+  let rep = getRepository("Commessa");
+  let toremove = await rep.findOne(id);
+  await rep.remove(toremove);
+  location.reload();
+}
+
+async function addCommessa() {
+  var commessa = {
+    matricola:   "12345",
+    tipo:        "TSI",
+    cliente:     "Nestlè",
+    riferimento: "Waterfall"
+  }
+  await getRepository("Commessa").save(commessa);
+  location.reload();
+}
