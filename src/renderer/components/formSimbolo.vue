01c1c3df18645b6ddac9f1ff30d2cb46afb43566
alv67 <alv67.web@gmail.com>
2019-05-17 23:21:47 +0200

    Pagina Simboli: ora il form è creato tramite Componente VUE

---
+++ src/renderer/components/formSimbolo.vue
@@ -0,0 +1,150 @@
+<template>
+  <q-card class="formCard">
+    <q-card-section class="row items-center">
+      <q-toolbar class="bg-primary glossy text-white">
+        <q-toolbar-title id="frmTitle">{{title}}</q-toolbar-title>
+        <q-btn flat round v-close-popup>&times;</q-btn>
+      </q-toolbar>
+      <q-space />
+    </q-card-section>
+
+    <q-separator />
+    <q-card-section>
+
+    <q-form
+      @submit="btnSave"
+      class="q-gutter-md">
+
+      <input v-model="frmID" style="display: none" id="frmId">
+      <q-input
+        filled
+        v-model="frmSigla"
+        label="Sigla"
+        :rules="[val => validaSigla()]" />
+      
+      <q-input
+        filled
+        v-model="frmDescrizione"
+        label="Descrizione" />
+
+      <div class="row">
+        <q-btn label="Salva" type="submit" color="secondary"></q-btn>
+        <q-btn v-if="isEditing" label="Undo" @click="btnUndo()" color="secondary" flat class="q-ml-sm" ></q-btn>
+        <q-space />
+        <q-btn v-if="isEditing" label="Elimina" @click.prevent="deleteConfirm = true" color="negative"></q-btn>
+      </div>
+    </q-form>
+    </q-card-section>
+    <q-dialog v-model="deleteConfirm" persistent>
+      <q-card>
+        <q-card-section>
+          <span class="q-ml-sm">Confermi eliminazione del simbolo: {{frmSigla}} ?</span>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Annulla" color="primary" v-close-popup />
+          <q-btn flat label="Conferma" color="primary" v-close-popup @click="btnDelete()"/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </q-card>
+</template>
+
+<script>
+const { Simbolo } = require('../../../models/specifiche_db/entity/Simbolo') 
+const { Descrizione } = require('../../../models/specifiche_db/entity/Descrizione');
+
+
+export default {
+
+  name: 'formSimbolo',
+  props: [
+    'simboloID',
+  ],   
+  data () {
+    return {
+      title: "",
+      isEditing: false,
+      frmID: null,
+      frmSigla: null,
+      frmDescrizione: null,
+      deleteConfirm: false
+    }
+  },
+//  watch: {
+//    simboloID: async function (value) {
+//      // this.loadForm()
+//    },
+//  },
+
+  created: async function () {
+    this.loadForm()
+  },
+  methods: {
+    btnSave: async function (event) {
+            // Check for correctness of values
+      let simb = (this.frmID === null) ? new Simbolo : await Simbolo.findOne({id: this.frmID})
+      let desc = (this.frmID === null) ? new Descrizione : simb.descrizione
+      desc.testo = this.frmDescrizione
+      await desc.save()
+      simb.descrizione = desc
+      simb.sigla = this.frmSigla
+      await simb.save()
+      this.$q.notify("Salvato Simbolo: " + simb.id + " - " + simb.sigla)
+      this.$emit("save")
+    },
+    btnDelete: async function (event) {      
+      if (this.frmID !== null){        
+        let simb = await Simbolo.findOne({id: this.frmID})
+        let desc = simb.descrizione
+        let sigla = simb.sigla
+        await simb.remove()
+        await desc.remove()
+        this.$q.notify("Eliminato simbolo: " + sigla + " !!")
+        this.$emit('delete')
+      }
+    },
+    btnUndo: async function () {
+      this.loadForm()
+    },
+    validaSigla: async function () {
+      if (this.frmSigla){
+        let stessaSigla = await Simbolo.findOne({sigla: this.frmSigla})
+        if (!stessaSigla) {
+          return true
+        } else if (stessaSigla.id != this.frmID) {
+          return 'Sigla già presente!'
+        } else {
+          return true
+        }
+      } else { 
+        return 'Campo obbligatorio!'
+      }
+    } ,
+    loadForm: async function () {
+      if (this.simboloID !== -1) { // Nuovo Simbolo
+        let simb = await Simbolo.findOne({id: this.simboloID})
+        this.title = "Modifica Simbolo"
+        this.frmID = simb.id
+        this.frmSigla = simb.sigla
+        this.frmDescrizione = simb.descrizione.testo
+        this.isEditing = true
+      } else {
+        this.title = "Aggiunta Simbolo"
+        this.frmID = null
+        this.frmSigla = null,
+        this.frmDescrizione = null
+        this.isEditing = false
+      }
+    },
+  }
+}
+</script>
+
+
+<style scoped>
+.formCard {
+  width: 100%;
+  max-width: 600px;
+}
+</style>
--- src/renderer/pages/simboli.vue
+++ src/renderer/pages/simboli.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="q-pa-md">
-    
+  <div class="q-pa-md">    
     <q-markup-table flat bordered>
       <thead class="bg-blue">
         <tr >
@@ -26,63 +25,34 @@
     </q-markup-table>
     <q-btn color="primary" @click="newSimbolo()" icon="far fa-plus-square" label="Simbolo"></q-btn>
     
-
-    <!-- Form modale per inserimento -->
-    <q-dialog 
-      v-model="editForm" 
-      transition-show="slide-down" 
-      transition-hide="slide-up">
-      <q-card class="formCard">
-        <q-card-section class="row items-center">
-          <q-toolbar class="bg-primary glossy text-white">
-            <q-toolbar-title id="frmTitle">{{formTitle}}</q-toolbar-title>
-            <q-btn flat round v-close-popup>&times;</q-btn>
-          </q-toolbar>
-          <q-space />
-        </q-card-section>
-
-        <q-separator />
-        <q-card-section>
-
-        <q-form
-          @submit="btnSave"
-          class="q-gutter-md">
-
-          <input v-model="frmID" style="display: none" id="frmId">
-          <q-input
-            filled
-            v-model="frmSigla"
-            label="Sigla"
-            :rules="[val => validaSigla()]" />
-          
-          <q-input
-            filled
-            v-model="frmDescrizione"
-            label="Descrizione" />
-
-          <div class="row">
-            <q-btn label="Salva" type="submit" color="secondary"></q-btn>
-            <q-btn v-if="isEditingMode" label="Undo" @click="btnUndo()" color="secondary" flat class="q-ml-sm" ></q-btn>
-            <q-space />
-            <q-btn v-if="isEditingMode" label="Elimina" @click.prevent="btnDelete($event)" color="negative"></q-btn>
-          </div>
-        </q-form>
-        </q-card-section>
-      </q-card>
+    <!-- Form inserimento tramite l'usp del componente 'formSimbolo' -->
+    <q-dialog v-model="editForm" 
+      transition-show = "slide-down" 
+      transition-hide = "slide-up">
+      <!-- Forza la creazione del componente con l'uso del v-if 
+        --  così viene ricreata da nuovo e elimita le latenze
+        --  dovute ai tempi di aggiornamento del parametro simboloID -->
+      <form-simbolo
+          v-if="editForm"
+          :simboloID = "selectedSimbolo" 
+          @save = "editForm = false; updateTable()"
+          @delete = "editForm = false; updateTable()">
+      </form-simbolo>  
     </q-dialog>
   </div>
 </template>
 
 
 <script>
-import { async } from 'q';
+import formSimbolo from '../components/formSimbolo.vue'
+
 const { createConnection, getRepository, getConnection, getConnectionOptions} = require('typeorm')
 const { Simbolo } = require('../../../models/specifiche_db/entity/Simbolo');
 const { Descrizione } = require('../../../models/specifiche_db/entity/Descrizione');
 
 const databasePath = "database.sqlite"
 
-const connection = null
+var connection = null
 
 async function loadSimboli() {
   let simboli = await getConnection("default").getRepository("Simbolo").find();
@@ -94,93 +64,40 @@ export default {
     return {
       simboli: [],
       editForm: false,
-      formTitle: "",
-      frmID: null,
-      frmSigla: null,
-      frmDescrizione: null,
-      isEditingMode: false,
+      editFormOld: false,
+      selectedSimbolo: null,
     }
+  },
+  components: {
+    formSimbolo,
   },
   methods: {
     editSimbolo: async function (event, idSimbolo) {
-      //let idSimbolo = event.target.getAttribute('sid')
-      this.$q.notify('Edit Simbolo:' + idSimbolo)
-      let simb = await Simbolo.findOne({id: idSimbolo})
-      this.frmID = simb.id
-      this.frmSigla = simb.sigla
-      this.frmDescrizione = simb.descrizione.testo
-      this.formTitle = "Modifica Simbolo"
-      this.isEditingMode = true
+      this.selectedSimbolo = idSimbolo
       this.editForm = true
     },
     newSimbolo: async function (event) {
-      this.frmID = null
-      this.frmSigla = null
-      this.frmDescrizione = null
-      this.formTitle = "Aggiungi Simbolo"
-      this.isEditingMode = false
+      this.selectedSimbolo = -1
       this.editForm = true
-      this.simboli = await Simbolo.find()
     },
     btnCancel: function (event) {
       this.editForm = false
     },
-    btnSave: async function (event) {
-      // Check for correctness of values
-      let simb = (this.frmID === null) ? new Simbolo : await Simbolo.findOne({id: this.frmID})
-      let desc = (this.frmID === null) ? new Descrizione : simb.descrizione
-      desc.testo = this.frmDescrizione
-      await desc.save()
-      simb.sigla = this.frmSigla
-      simb.descrizione = desc
-      await simb.save()
-      this.editForm = false
+    updateTable: async function () {
       this.simboli = await Simbolo.find()
-    },
-    btnDelete: async function (event) {      
-      if (this.frmID !== null){
-        this.$q.notify('Elimino Simbolo:' + this.frmID)
-        let simb = await Simbolo.findOne({id: this.frmID})
-        let desc = simb.descrizione
-        alert("Elimino dal database : "+ simb.id + ": " + simb.sigla + " ?")
-        await simb.remove()
-        await desc.remove()
-      }
-      this.editForm = false
-      this.simboli = await Simbolo.find()
-    },
-    btnUndo: async function () {
-      let simb = await Simbolo.findOne({id: this.frmID})
-      this.frmSigla = simb.sigla
-      this.frmDescrizione = simb.descrizione.testo
-    },
-    validaSigla: async function () {
-      if (this.frmSigla){
-        let stessaSigla = await Simbolo.findOne({sigla: this.frmSigla})
-        if (!stessaSigla) {
-          return true
-        } else if (stessaSigla.id != this.frmID) {
-          return 'Sigla già presente!'
-        } else {
-          return true
-        }
-      } else { 
-        return 'Campo obbligatorio!'
-      }
     }
-
   },
   created () {
     if (!connection) {
       getConnectionOptions("default").then(async connectionOptions => {
         Object.assign(connectionOptions, {database: databasePath});
         Object.assign(connectionOptions, {entities: [Simbolo, Descrizione]});
-        const connection = await createConnection(connectionOptions);
-      }).then( async () => {
-        this.simboli = await Simbolo.find();
+        connection = await createConnection(connectionOptions);
+      }).then(() => {
+        this.updateTable()
       })
     } else {
-        (async () => {this.simboli = await Simbolo.find()})()
+        this.updateTable()
     }
   } 
 }
